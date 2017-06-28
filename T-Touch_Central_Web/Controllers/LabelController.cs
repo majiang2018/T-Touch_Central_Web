ee77bc7fbabaad932243503e069b2d2d60d6add0
User <User@RD-19>
2017-06-28 16:35:58 +0800

    增加了Label

---
+++ T-Touch_Central_Web/Controllers/LabelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T_Touch_Central_Web.Controllers
+{
+    public class LabelController : Controller
+    {
+        // GET: Label
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
