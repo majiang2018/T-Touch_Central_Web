a7fd8128e6d9fb4a2cfa18795d3c02def28e7f5a
User <User@RD-19>
2017-06-29 09:39:43 +0800

    修改细节

---
+++ T-Touch_Central_Web/Controllers/LabelDesignController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace T_Touch_Central_Web.Controllers
+{
+    public class LabelDesignController : Controller
+    {
+        // GET: LabelDesign
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
