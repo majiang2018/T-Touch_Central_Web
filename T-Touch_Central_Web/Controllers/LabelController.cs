1e24a5f34e3ab56888c4e82d7e9e2cc13766af3c
User <User@RD-19>
2017-07-11 12:49:10 +0800

    增加标签下发

--- T-Touch_Central_Web/Controllers/LabelController.cs
+++ T-Touch_Central_Web/Controllers/LabelController.cs
@@ -1,6 +1,13 @@
 ﻿using DATA;
+using Ionic.Zip;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
 using System.Web.Mvc;
 
 namespace T_Touch_Central_Web.Controllers
@@ -31,7 +38,7 @@ namespace T_Touch_Central_Web.Controllers
                 {
                     foreach (var item in Id.Split(',').ToArray())
                     {
-                        if (System.IO.File.Exists(path+"\\"+item))
+                        if (System.IO.File.Exists(path + "\\" + item))
                         {
                             System.IO.File.Delete(path + "\\" + item);
                         }
@@ -60,7 +67,100 @@ namespace T_Touch_Central_Web.Controllers
         [HttpPost]
         public string DownLoad(string Id, string Ip)
         {
+            var db = new DB();
             var result = string.Empty;
+            var result1 = string.Empty;
+            string zipPath = string.Empty;
+            if (Id != "")
+            {
+                if (Id.Split(',').ToArray().Length == 1)
+                {
+                    if (Id.Contains(".zip") || Ip.Contains(".ZIP"))
+                    {
+                        zipPath = Server.MapPath("~/Uploads") + @"\" + Id;
+                    }
+                    else if (Id.Contains(".fmt")|| Id.Contains(".FMT"))
+                    {
+                        zipPath = Server.MapPath("~/Uploads") + @"\data-" + DateTime.Now.ToFileTimeUtc() + ".zip";
+                        using (ZipFile zip = new ZipFile())
+                        {
+                            foreach (var item in Id.Split(',').ToArray())
+                            {
+                                zip.AddFile(Server.MapPath("~/Uploads") + @"\" + Id, "format_label");
+                            }
+                            zip.Save(zipPath);
+                        }
+                    }
+                }
+                else if (Id.Split(',').ToArray().Length>1 )
+                {
+                    zipPath = Server.MapPath("~/Uploads") + @"\data-" + DateTime.Now.ToFileTimeUtc() + ".zip";
+                    using (ZipFile zip = new ZipFile())
+                    {
+                        foreach (var item in Id.Split(',').ToArray())
+                        {
+                            zip.AddFile(Server.MapPath("~/Uploads") + @"\" + item, "format_label");
+                        }
+                        zip.Save(zipPath);
+                    }
+                }
+                else
+                {
+                    result = "文件错误！";
+                    return result;
+                }
+                if (Ip != "")
+                {
+                    foreach (var item in Ip.Split(',').ToArray())
+                    {
+                        var Sql = db.Scales.SingleOrDefault(x => x.Id == int.Parse(item));
+                        try
+                        {
+                            //测试IP
+                            Ping pingSender = new Ping();
+                            PingOptions options = new PingOptions();
+                            string data = "";
+                            byte[] buffer = Encoding.ASCII.GetBytes(data);
+                            int timeout = 120;
+                            PingReply reply = pingSender.Send(Sql.IpAddress, timeout, buffer, options);
+                            if (reply.Status == IPStatus.Success)
+                            {
+                                TcpClient tcpClient = new TcpClient(Sql.IpAddress, 1000);
+                                NetworkStream networkStream = tcpClient.GetStream();
+                                //打开文件流
+                                using (FileStream fs = new FileStream(zipPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                                {
+                                    string filename = Path.GetFileName(zipPath);
+                                    byte[] sendBytes = Encoding.Default.GetBytes("set:data:" + filename + ":" + fs.Length.ToString() + "\r\n");
+                                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                                    byte[] buffer1 = new byte[1024];
+                                    int read, sent = 0;
+                                    while ((read = fs.Read(buffer1, 0, 1024)) != 0)
+                                    {
+                                        networkStream.Write(buffer1, 0, buffer1.Length);
+                                        sent += read;
+                                    }
+                                    fs.Close();
+                                }
+                                result += Sql.IpAddress + ":下载成功！" + Environment.NewLine;
+
+                            }
+                            else
+                            {
+                                result += Sql.IpAddress + ":网络断线！" + Environment.NewLine;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            result += Sql.IpAddress + ":" + ex.Message + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+            if (System.IO.File.Exists(zipPath))
+            {
+                System.IO.File.Delete(zipPath);
+            }
             return result;
         }
     }
